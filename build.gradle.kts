3837f324c5519969c4d01d780efc8048a5e93247
intisy (Finn Birich) <finn@birich.de>
2026-09-23 11:23:57 +0200

    chore: point ci and build at development and main

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 group = "io.github.thebusybiscuit"
 description = "ExtraGear is a Slimefun addon that adds extra armor sets and tools."
 
-apply(from = "https://raw.githubusercontent.com/Slimefun5/gradle/stable/slimefun-addon.gradle")
+apply(from = "https://raw.githubusercontent.com/Slimefun5/gradle/main/slimefun-addon.gradle")
 
 dependencies {
     githubImplementation("Slimefun5:SlimefunMetrics:v1.0.0")
